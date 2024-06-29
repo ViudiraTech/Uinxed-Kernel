8eac96c60c4125929d7f503c2c606c11c4e58fa4
MicroFish <2609948707@qq.com>
2024-06-29 21:19:25 +0800

    更新了板载蜂鸣器驱动

---
+++ include/beep.h
@@ -0,0 +1,10 @@
+// beep.h -- 板载蜂鸣器驱动头文件（基于 GPL-3.0 开源协议）
+// Copyright © 2020 ViudiraTech，保留所有权利。
+// 源于 MicroFish 撰写于 2024-6-29.
+
+#ifndef INCLUDE_BEEP_H_
+#define INCLUDE_BEEP_H_
+
+void system_beep(int hertz); // 设置板载蜂鸣器状态
+
+#endif // INCLUDE_BEEP_H_
