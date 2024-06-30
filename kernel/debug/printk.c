83f42b4300ecc3f1f4c9325511f5627e856fe179
ViudiraTech <2609948707@qq.com>
2024-06-30 20:38:47 +0800

    更新了一个print_info

--- kernel/debug/printk.c
+++ kernel/debug/printk.c
@@ -9,21 +9,32 @@
 
 static int vsprintf(char *buff, const char *format, va_list args);
 
-void print_succ(char *str){
+void print_succ(char *str)
+{
 	printk_color(rc_black, rc_white, "[");
-	printk_color(rc_black, rc_green, "SUCCESS");
+	printk_color(rc_black, rc_green, " SUCCESS ");
 	printk_color(rc_black, rc_white, "]");
 	printk(" %s\n", str);
 }
 
-void print_warn(char *str){
+void print_info(char *str)
+{
 	printk_color(rc_black, rc_white, "[");
-	printk_color(rc_black, rc_light_brown, "WARNING");
+	printk_color(rc_black, rc_white, " INFO ");
 	printk_color(rc_black, rc_white, "]");
 	printk(" %s\n", str);
 }
 
-void print_erro(char *str){
+void print_warn(char *str)
+{
+	printk_color(rc_black, rc_white, "[");
+	printk_color(rc_black, rc_light_brown, " WARNING ");
+	printk_color(rc_black, rc_white, "]");
+	printk(" %s\n", str);
+}
+
+void print_erro(char *str)
+{
 	printk_color(rc_black, rc_white, "[");
 	printk_color(rc_black, rc_red, " ERROR ");
 	printk_color(rc_black, rc_white, "]");
