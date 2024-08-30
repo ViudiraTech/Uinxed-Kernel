1c25f262146ea794809ec0aef6944eee4e9f97e5
ViudiraTech <2609948707@qq.com>
2024-08-30 20:08:16 +0800

    版本号错位，更新了版本号

--- include/uinxed.h
+++ include/uinxed.h
@@ -15,7 +15,7 @@
 #define BUILD_DATE __DATE__
 #define BUILD_TIME __TIME__
 #define KERNL_VERS "v0.0.2408300" // Version format: v[VERSION].[PACK].YYMMDDF
-#define KERNL_BUID 69
+#define KERNL_BUID 71
 #define PROJK_COPY "Copyright 2020 ViudiraTech. All Rights Reserved."
 
 #endif // INCLUDE_UINXED_H_
