1c2404680a83de742d0f791bed3d3f84baa4f60d
ViudiraTech <2609948707@qq.com>
2024-09-16 16:20:13 +0800

    增加了BMP解析

--- include/uinxed.h
+++ include/uinxed.h
@@ -14,8 +14,8 @@
 
 #define BUILD_DATE __DATE__
 #define BUILD_TIME __TIME__
-#define KERNL_VERS "v0.0.2409161" // Version format: v[VERSION].[PACK].YYMMDDF
-#define KERNL_BUID 99
+#define KERNL_VERS "v0.0.2409162" // Version format: v[VERSION].[PACK].YYMMDDF
+#define KERNL_BUID 101
 #define PROJK_COPY "Copyright 2020 ViudiraTech. All Rights Reserved."
 
 #endif // INCLUDE_UINXED_H_
