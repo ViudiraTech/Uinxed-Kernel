d91df0aecdb16280f32e8769d567d09a12a66c38
ViudiraTech <2609948707@qq.com>
2024-10-06 08:51:27 +0000

    update include/uinxed.h.
    Signed-off-by: ViudiraTech <2609948707@qq.com>

--- include/uinxed.h
+++ include/uinxed.h
@@ -15,7 +15,7 @@
 #define BUILD_DATE __DATE__
 #define BUILD_TIME __TIME__
 #define KERNL_VERS "v0.0.2410030" // Version format: v[VERSION].[PACK].YYMMDDF
-#define KERNL_BUID 118
+#define KERNL_BUID 120
 #define PROJK_COPY "Copyright 2020 ViudiraTech. All Rights Reserved."
 
 #endif // INCLUDE_UINXED_H_
