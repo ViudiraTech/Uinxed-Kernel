5a185a0fae124302a28395a5ffddbdb904f7f381
ViudiraTech <2609948707@qq.com>
2024-11-02 15:02:35 +0800

    更新了OST

--- include/uinxed.h
+++ include/uinxed.h
@@ -15,7 +15,7 @@
 #define BUILD_DATE __DATE__
 #define BUILD_TIME __TIME__
 #define KERNL_VERS "v0.0.2411022" // Version format: v[VERSION].[PACK].YYMMDDF
-#define KERNL_BUID 167
+#define KERNL_BUID 168
 #define PROJK_COPY "Copyright 2020 ViudiraTech. All Rights Reserved."
 
 #endif // INCLUDE_UINXED_H_
