49e64bdecbfe71585cc22b17fa9976ff9441b029
Viudira <2609948707@qq.com>
2024-11-11 01:03:29 +0800

    Update uinxed.h

--- include/uinxed.h
+++ include/uinxed.h
@@ -14,8 +14,8 @@
 
 #define BUILD_DATE __DATE__
 #define BUILD_TIME __TIME__
-#define KERNL_VERS "v0.0.2411103" // Version format: v[VERSION].[PACK].YYMMDDF
-#define KERNL_BUID 196
+#define KERNL_VERS "v0.0.2411110" // Version format: v[VERSION].[PACK].YYMMDDF
+#define KERNL_BUID 199
 #define PROJK_COPY "Copyright 2020 ViudiraTech. All Rights Reserved."
 
 #endif // INCLUDE_UINXED_H_
