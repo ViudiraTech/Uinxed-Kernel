66f46c61a79468d1c42592efe47ce73bf7201884
Rainy101112 <136242076+Rainy101112@users.noreply.github.com>
2025-01-05 12:34:27 +0800

    Update uinxed.h

--- include/uinxed.h
+++ include/uinxed.h
@@ -14,8 +14,8 @@
 
 #define BUILD_DATE __DATE__
 #define BUILD_TIME __TIME__
-#define KERNL_VERS "v0.0.2501053" // Version format: v[VERSION].[PACK].YYMMDDF
-#define KERNL_BUID 309
+#define KERNL_VERS "v0.0.2501055" // Version format: v[VERSION].[PACK].YYMMDDF
+#define KERNL_BUID 311
 #define PROJK_COPY "Copyright 2020 ViudiraTech. All Rights Reserved."
 
 #define SHOW_START_LOGO
