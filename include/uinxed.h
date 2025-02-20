23fe4026899910b06df6d78265c85372a82e4e35
MicroFish <2609948707@qq.com>
2025-02-20 22:09:02 +0800

    Update uinxed.h

--- include/uinxed.h
+++ include/uinxed.h
@@ -15,7 +15,7 @@
 #define BUILD_DATE __DATE__
 #define BUILD_TIME __TIME__
 #define KERNL_VERS "v0.0.2502012" // Version format: v[VERSION].[PACK].YYMMDDF
-#define KERNL_BUID 341
+#define KERNL_BUID 343
 #define PROJK_COPY "Copyright 2020 ViudiraTech. Open all rights."
 
 #endif // INCLUDE_UINXED_H_
