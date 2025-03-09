4c58befa01bf89c8a1bb0583a88ea2d7a3feed77
MicroFish <2609948707@qq.com>
2025-03-09 12:31:18 +0800

    更新了32位SMBIOS

--- include/smbios.h
+++ include/smbios.h
@@ -14,26 +14,47 @@
 
 #include "stdint.h"
 
-/* 入口点结构 */
+/* 32位入口点结构 */
+struct EntryPoint32 {
+	uint8_t AnchorString[4];				// 锚字符串，值为"_SM_"
+	uint8_t Checksum;						// 校验和
+	uint8_t EntryLength;					// 入口点结构长度
+	uint8_t MajorVersion;					// SMBIOS主版本号
+	uint8_t MinorVersion;					// SMBIOS次版本号
+	uint16_t MaxStructureSize;				// 最大结构体大小
+	uint8_t EntryPointRevision;				// 入口点修订号
+	uint8_t FormattedArea[5];				// 格式化区域
+	uint8_t IntermediateAnchorString[5];	// 中间锚字符串，值为"_DMI_"
+	uint8_t IntermediateChecksum;			// 中间校验和
+	uint16_t StructureTableLength;			// 结构表长度
+	uint32_t StructureTableAddress;			// 结构表地址
+	uint16_t NumberOfStructures;			// 结构数量
+	uint8_t BCDRevision;					// BCD 修订号
+};
+
+/* 64位入口点结构 */
 struct EntryPoint64 {
-	char AnchorString[5];			// 锚字符串，64位为"_SM3_"
-	uint8_t EntryPointChecksum;		// 入口点校验和
-	uint8_t EntryPointLength;		// 入口点结构长度
-	uint8_t SMBIOSMajorVersion;		// SMBIOS主版本
-	uint8_t SMBIOSMinorVersion;		// SMBIOS次版本
-	uint8_t SMBIOSDocrev;			// 文档修订版
-	uint8_t EntryPointRevision;		// 入口点修订版
-	uint8_t Reserved;				// 保留字节
-	uint32_t StructureTableMaxSize;	// 结构表最大大小
-	uint64_t StructureTableAddress;	// 结构表地址
+	uint8_t AnchorString[5];				// 锚字符串，值为"_SM3_"
+	uint8_t Checksum;						// 校验和
+	uint8_t EntryLength;					// 入口点结构长度
+	uint8_t MajorVersion;					// SMBIOS主版本号
+	uint8_t MinorVersion;					// SMBIOS次版本号
+	uint8_t Docrev;							// 文档修订号
+	uint8_t EntryPointRevision;				// 入口点修订号
+	uint8_t Reserved;						// 保留字节
+	uint32_t MaxStructureSize;				// 最大结构体大小
+	uint64_t StructureTableAddress;			// 结构表地址
 };
 
 /* 结构表头部 */
 struct Header {
-	uint8_t type;		// 结构类型
-	uint8_t length;		// 结构长度（不包括字符串表）
-	uint16_t handle;	// 结构句柄
+	uint8_t type;							// 结构类型
+	uint8_t length;							// 结构长度（不包括字符串表）
+	uint16_t handle;						// 结构句柄
 };
+
+/* 获取SMBIOS入口点 */
+void *smbios_entry(void);
 
 /* 获取SMBIOS主版本 */
 int smbios_major_version(void);
--- kernel/chipset/smbios.c
+++ kernel/chipset/smbios.c
@@ -17,16 +17,32 @@ static __volatile__ struct limine_smbios_request smbios_request = {
 	.id = LIMINE_SMBIOS_REQUEST
 };
 
+/* 获取SMBIOS入口点 */
+void *smbios_entry(void)
+{
+	if (smbios_request.response == 0)
+		return 0;
+	if (smbios_request.response->entry_64)
+		return (void *)smbios_request.response->entry_64;
+	else if (smbios_request.response->entry_32)
+		return (void *)smbios_request.response->entry_32;
+	return 0;
+}
+
 /* 获取SMBIOS主版本 */
 int smbios_major_version(void)
 {
-	struct EntryPoint64 *entry = (struct EntryPoint64 *)smbios_request.response->entry_64;
-	return entry->SMBIOSMajorVersion;
+	if (smbios_request.response->entry_64)
+		return ((struct EntryPoint64 *)smbios_entry())->MajorVersion;
+	else
+		return ((struct EntryPoint32 *)smbios_entry())->MajorVersion;
 }
 
 /* 获取SMBIOS次版本 */
 int smbios_minor_version(void)
 {
-	struct EntryPoint64 *entry = (struct EntryPoint64 *)smbios_request.response->entry_64;
-	return entry->SMBIOSMinorVersion;
+	if (smbios_request.response->entry_64)
+		return ((struct EntryPoint64 *)smbios_entry())->MinorVersion;
+	else
+		return ((struct EntryPoint32 *)smbios_entry())->MajorVersion;
 }
