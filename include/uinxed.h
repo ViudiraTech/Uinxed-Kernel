b24389bb5da0553c54e84f58e30cc27ca3dd0324
MicroFish <2609948707@qq.com>
2025-04-12 12:40:20 +0800

    Update uinxed.h

--- include/uinxed.h
+++ include/uinxed.h
@@ -14,7 +14,7 @@
 
 #define BUILD_DATE __DATE__			// Compilation Date
 #define BUILD_TIME __TIME__			// Compile time
-#define KERNL_VERS "0.0.250411"		// Version format: v[major version].[patch version].[YY-MM-DD]
+#define KERNL_VERS "0.0.250412"		// Version format: v[major version].[patch version].[YY-MM-DD]
 
 /* Compiler judgment */
 #if defined(__clang__)
