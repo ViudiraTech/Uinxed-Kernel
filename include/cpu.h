da9bcc1042d595634420dee9b5da55e0b0b870f2
MicroFish <2609948707@qq.com>
2025-07-04 18:44:39 +0800

    Add more CPUID functions.

--- include/cpu.h
+++ include/cpu.h
@@ -126,7 +126,7 @@ enum cpuid_requests {
 };
 
 /* Get CPUID */
-void cpuid(uint32_t code, uint32_t *a, uint32_t *b, uint32_t *c, uint32_t *d);
+void cpuid(uint32_t code, uint32_t *eax, uint32_t *ebx, uint32_t *ecx, uint32_t *edx);
 
 /* Get CPU manufacturer name */
 char *get_vendor_name(void);
@@ -142,5 +142,56 @@ uint32_t get_cpu_virt_bits(void);
 
 /* Check CPU supports NX/XD */
 int cpu_supports_nx(void);
+
+/* Check CPU supports 64bit */
+int cpu_support_64bit(void);
+
+/* Check CPU supports MMX */
+int cpu_support_mmx(void);
+
+/* Check CPU supports SSE */
+int cpu_support_sse(void);
+
+/* Check CPU supports SSE2 */
+int cpu_support_sse2(void);
+
+/* Check CPU supports SSE3 */
+int cpu_support_sse3(void);
+
+/* Check CPU supports SSSE3 */
+int cpu_support_ssse3(void);
+
+/* Check CPU supports SSE4.1 */
+int cpu_support_sse41(void);
+
+/* Check CPU supports SSE4.2 */
+int cpu_support_sse42(void);
+
+/* Check CPU supports SSE4a (AMD-specific) */
+int cpu_support_sse4a(void);
+
+/* Check CPU supports XOP (AMD-specific) */
+int cpu_support_xop(void);
+
+/* Check CPU supports FMA4 (AMD-specific) */
+int cpu_support_fma4(void);
+
+/* Check CPU supports CVT16 */
+int cpu_support_cvt16(void);
+
+/* Check CPU supports AVX */
+int cpu_support_avx(void);
+
+/* Check CPU supports AVX2 */
+int cpu_support_avx2(void);
+
+/* Get core frequency */
+uint32_t get_core_freq(void);
+
+/* Get tsc frequency */
+uint32_t get_tsc_freq(void);
+
+/* Get bus frequency */
+uint32_t get_bus_freq(void);
 
 #endif // INCLUDE_CPU_H_
--- kernel/chipset/cpu.c
+++ kernel/chipset/cpu.c
@@ -14,9 +14,9 @@
 #include "printk.h"
 
 /* Get CPUID */
-void cpuid(uint32_t code, uint32_t *a, uint32_t *b, uint32_t *c, uint32_t *d) // NOLINT
+void cpuid(uint32_t code, uint32_t *eax, uint32_t *ebx, uint32_t *ecx, uint32_t *edx) // NOLINT
 {
-    __asm__ volatile("cpuid" : "=a"(*a), "=b"(*b), "=c"(*c), "=d"(*d) : "a"(code) : "memory");
+    __asm__ volatile("cpuid" : "=a"(*eax), "=b"(*ebx), "=c"(*ecx), "=d"(*edx) : "a"(code) : "memory");
 }
 
 /* Get CPU manufacturer name */
@@ -45,23 +45,159 @@ char *get_model_name(void)
 /* Get the CPU physical address size */
 uint32_t get_cpu_phys_bits(void)
 {
-    uint32_t rax, rbx, rcx, rdx;
-    cpuid(0x80000008, &rax, &rbx, &rcx, &rdx);
-    return rax & 0xff;
+    uint32_t eax, ebx, ecx, edx;
+    cpuid(0x80000008, &eax, &ebx, &ecx, &edx);
+    return (eax & 0xff);
 }
 
 /* Get CPU virtual address size */
 uint32_t get_cpu_virt_bits(void)
 {
-    uint32_t rax, rbx, rcx, rdx;
-    cpuid(0x80000008, &rax, &rbx, &rcx, &rdx);
-    return (rax >> 8) & 0xff;
+    uint32_t eax, ebx, ecx, edx;
+    cpuid(0x80000008, &eax, &ebx, &ecx, &edx);
+    return ((eax >> 8) & 0xff);
 }
 
 /* Check CPU supports NX/XD */
 int cpu_supports_nx(void)
 {
-    uint32_t rax, rbx, rcx, rdx;
-    cpuid(0x80000001, &rax, &rbx, &rcx, &rdx);
-    return (rdx & (1 << 20)) != 0;
+    uint32_t eax, ebx, ecx, edx;
+    cpuid(0x80000001, &eax, &ebx, &ecx, &edx);
+    return ((edx & (1 << 20)) != 0);
+}
+
+/* Check CPU supports 64bit */
+int cpu_support_64bit(void)
+{
+    uint32_t eax, ebx, ecx, edx;
+    cpuid(0x80000001, &eax, &ebx, &ecx, &edx);
+    return (edx & (1 << 29) != 0);
+}
+
+/* Check CPU supports MMX */
+int cpu_support_mmx(void)
+{
+    uint32_t eax, ebx, ecx, edx;
+    cpuid(0x00000001, &eax, &ebx, &ecx, &edx);
+    return ((edx & (1 << 23)) != 0);
+}
+
+/* Check CPU supports SSE */
+int cpu_support_sse(void)
+{
+    uint32_t eax, ebx, ecx, edx;
+    cpuid(0x00000001, &eax, &ebx, &ecx, &edx);
+    return ((edx & (1 << 25)) != 0);
+}
+
+/* Check CPU supports SSE2 */
+int cpu_support_sse2(void)
+{
+    uint32_t eax, ebx, ecx, edx;
+    cpuid(0x00000001, &eax, &ebx, &ecx, &edx);
+    return ((edx & (1 << 26)) != 0);
+}
+
+/* Check CPU supports SSE3 */
+int cpu_support_sse3(void)
+{
+    uint32_t eax, ebx, ecx, edx;
+    cpuid(0x00000001, &eax, &ebx, &ecx, &edx);
+    return ((ecx & (1 << 0)) != 0);
+}
+
+/* Check CPU supports SSSE3 */
+int cpu_support_ssse3(void)
+{
+    uint32_t eax, ebx, ecx, edx;
+    cpuid(0x00000001, &eax, &ebx, &ecx, &edx);
+    return ((ecx & (1 << 9)) != 0);
+}
+
+/* Check CPU supports SSE4.1 */
+int cpu_support_sse41(void)
+{
+    uint32_t eax, ebx, ecx, edx;
+    cpuid(0x00000001, &eax, &ebx, &ecx, &edx);
+    return ((ecx & (1 << 19)) != 0);
+}
+
+/* Check CPU supports SSE4.2 */
+int cpu_support_sse42(void)
+{
+    uint32_t eax, ebx, ecx, edx;
+    cpuid(0x00000001, &eax, &ebx, &ecx, &edx);
+    return ((ecx & (1 << 20)) != 0);
+}
+
+/* Check CPU supports SSE4a (AMD specific) */
+int cpu_support_sse4a(void)
+{
+    uint32_t eax, ebx, ecx, edx;
+    cpuid(0x80000001, &eax, &ebx, &ecx, &edx); // SSE4a info in extended leaf
+    return ((ecx & (1 << 6)) != 0);
+}
+
+/* Check CPU supports XOP (AMD specific) */
+int cpu_support_xop(void)
+{
+    uint32_t eax, ebx, ecx, edx;
+    cpuid(0x80000001, &eax, &ebx, &ecx, &edx);
+    return ((ecx & (1 << 11)) != 0);
+}
+
+/* Check CPU supports FMA4 (AMD specific) */
+int cpu_support_fma4(void)
+{
+    uint32_t eax, ebx, ecx, edx;
+    cpuid(0x80000001, &eax, &ebx, &ecx, &edx);
+    return ((ecx & (1 << 16)) != 0);
+}
+
+/* Check CPU supports CVT16 */
+int cpu_support_cvt16(void)
+{
+    uint32_t eax, ebx, ecx, edx;
+    cpuid(0x00000001, &eax, &ebx, &ecx, &edx);
+    return ((ecx & (1 << 29)) != 0);
+}
+
+/* Check CPU supports AVX */
+int cpu_support_avx(void)
+{
+    uint32_t eax, ebx, ecx, edx;
+    cpuid(0x00000001, &eax, &ebx, &ecx, &edx);
+    return ((ecx & (1 << 28)) != 0);
+}
+
+/* Check CPU supports AVX2 */
+int cpu_support_avx2(void)
+{
+    uint32_t eax, ebx, ecx, edx;
+    cpuid(0x00000007, &eax, &ebx, &ecx, &edx);
+    return ((ebx & (1 << 5)) != 0);
+}
+
+/* Get core frequency */
+uint32_t get_core_freq(void)
+{
+    uint32_t eax, ebx, ecx, edx;
+    cpuid(0x00000015, &eax, &ebx, &ecx, &edx);
+    return ecx;
+}
+
+/* Get tsc frequency */
+uint32_t get_tsc_freq(void)
+{
+    uint32_t eax, ebx, ecx, edx;
+    cpuid(0x00000015, &eax, &ebx, &ecx, &edx);
+    return ((ecx * ebx) / eax); 
+}
+
+/* Get bus frequency */
+uint32_t get_bus_freq(void)
+{
+    uint32_t eax, ebx, ecx, edx;
+    cpuid(0x00000016, &eax, &ebx, &ecx, &edx);
+    return ((ecx & 0xffff) * 1000000); 
 }
