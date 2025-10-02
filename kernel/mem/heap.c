9bb9efd0e13a8be37b71e955fdc726fd5c572820
W9pi3cZ1 <ghg13368@163.com>
2025-10-02 14:10:13 +0800

    Fix heap init crash when KVM is enabled.

--- kernel/mem/heap.c
+++ kernel/mem/heap.c
@@ -11,14 +11,14 @@
 
 #include "heap.h"
 #include "alloc.h"
+#include "cpuid.h"
 #include "frame.h"
 #include "hhdm.h"
-#include "limine.h"
 #include "page.h"
 #include "stddef.h"
 #include "stdint.h"
+#include "stdlib.h"
 #include "string.h"
-#include "uinxed.h"
 
 uint64_t heap_start = 0;
 uint64_t heap_size  = 0;
@@ -30,17 +30,16 @@ void init_heap(void)
     uint64_t min_heap_frame_count = MIN_HEAP_SIZE / PAGE_SIZE;
     heap_frame_count              = heap_frame_count > min_heap_frame_count ? heap_frame_count : min_heap_frame_count;
 
-    struct limine_memmap_response *memmap     = memmap_request.response;
-    struct limine_memmap_entry    *last_entry = memmap->entries[memmap->entry_count - 1];
-    pointer_cast_t                 heap_virt;
-    heap_virt.ptr = phys_to_virt(last_entry->base + last_entry->length);
+    pointer_cast_t heap_virt;
+    heap_virt.ptr = phys_to_virt((uint64_t)1 << get_cpu_phys_bits()); // End of HHDM for heap
+    heap_virt.val = ALIGN_UP(heap_virt.val, 16);
 
     uint64_t heap_virt_start = heap_virt.val;
     heap_start               = heap_virt_start;
     heap_size                = heap_frame_count * PAGE_SIZE;
     page_map_range_to_random(get_kernel_pagedir(), heap_start, heap_size, KERNEL_PTE_FLAGS);
 
-    heap_init((uint8_t *)heap_start, heap_size);
+    heap_init(heap_virt.ptr, heap_size);
 }
 
 /* Allocate an empty memory */
