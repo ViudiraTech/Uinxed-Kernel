4d2db1931190f0410dd1b9b7ff55d245f2613943
MicroFish <2609948707@qq.com>
2025-10-06 22:20:23 +0800

    There is a problem with the heap algorithm, use a fixed heap address.

--- kernel/mem/heap.c
+++ kernel/mem/heap.c
@@ -20,8 +20,8 @@
 #include "stdlib.h"
 #include "string.h"
 
-uint64_t KERNEL_HEAP_START = 0;
-uint64_t KERNEL_HEAP_SIZE  = 0;
+uint64_t KERNEL_HEAP_START = 0xffff900000000000;
+uint64_t KERNEL_HEAP_SIZE  = 0x6400000;
 
 /* Initialize the memory heap */
 void init_heap(void)
